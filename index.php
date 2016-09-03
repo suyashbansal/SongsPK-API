9de76040131f1c6f6e5d3b1c29323ff98c4137bc
Suyash Bansal <suyash.bansal5@gmail.com>
2016-09-03 18:49:48 +0530

    Renamed the array

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $html = curl_exec ($curl);
 curl_close ($curl);
 
-$suyash = array();
+$slider = array();
 
 preg_match_all("/leftrightslide\[(\d)\]='(.*?)'/", $html, $matches, PREG_SET_ORDER);
 
@@ -13,7 +13,7 @@ foreach ($matches as $key) {
   preg_match_all("/href=\"(.*)\".*src=\"(.*)\".*alt=\"(.*)\"/", $key[2], $matches2, PREG_SET_ORDER);
   unset($matches2[0][0]);
   //$suyash[] = $matches2[0];
-  $suyash[$key[1]] = array('link' => $matches2[0][1], 'image' => $matches2[0][2], 'name' => substr($matches2[0][3], 0, -10));
+  $slider[$key[1]] = array('link' => $matches2[0][1], 'image' => $matches2[0][2], 'name' => substr($matches2[0][3], 0, -10));
 }
-echo json_encode($suyash);
+echo json_encode($slider);
 ?>
